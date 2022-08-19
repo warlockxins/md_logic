b7e3a49d7d229806830464045b0fe2ecbcf02e8b
warlockxins <warlockxins@gmail.com>
2022-08-19 17:59:44 +0300

    all basic operands done - matches expression evaluator functionality

--- src/json_logic/mod.rs
+++ src/json_logic/mod.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
-use serde_json::{Result, Value};
+use serde_json::{Number, Result, Value};
+
+use crate::context::get_context_var;
 
 type OrderingOperation = Vec<AllCombined>;
 
@@ -17,52 +19,152 @@ enum Opss {
     MoreEq(OrderingOperation),
     #[serde(alias = "!=")]
     Neq(OrderingOperation),
+    #[serde(alias = "+")]
+    Plus(OrderingOperation),
+    #[serde(alias = "-")]
+    Minus(OrderingOperation),
+    #[serde(alias = "*")]
+    Multiply(OrderingOperation),
+    #[serde(alias = "/")]
+    Division(OrderingOperation),
+    #[serde(alias = "var")]
+    Var(String),
 }
 
-#[derive(Debug)]
-enum Operations {
-    More,
-    Less,
-    Eq,
-    Neq,
-    MoreEq,
-    LessEq,
+fn binary_op_vars(l: &Vec<AllCombined>, context: &Value) -> Option<Vec<AllCombined>> {
+    if l.len() < 2 {
+        return None;
+    }
+    Some(execute_combined_list(&l, context))
 }
 
 impl Opss {
-    fn execute(&self) -> AllCombined {
-        let (op, arguments) = match self {
-            Opss::Less(l) => (Operations::Less, l),
-            Opss::More(l) => (Operations::More, l),
-            Opss::Eq(l) => (Operations::Eq, l),
-            Opss::LessEq(l) => (Operations::LessEq, l),
-            Opss::MoreEq(l) => (Operations::MoreEq, l),
-            Opss::Neq(l) => (Operations::Neq, l),
-        };
-        if arguments.len() < 2 {
-            return AllCombined::Primitive(Value::Bool(false));
+    fn execute(&self, context: &Value) -> AllCombined {
+        match self {
+            Opss::Less(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    return AllCombined::Primitive(Value::Bool(&built_list[0] < &built_list[1]));
+                }
+                AllCombined::Primitive(Value::Bool(false))
+            }
+            Opss::More(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    return AllCombined::Primitive(Value::Bool(&built_list[0] > &built_list[1]));
+                }
+                AllCombined::Primitive(Value::Bool(false))
+            }
+            Opss::Eq(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    return AllCombined::Primitive(Value::Bool(&built_list[0] == &built_list[1]));
+                }
+                AllCombined::Primitive(Value::Bool(false))
+            }
+            Opss::LessEq(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    return AllCombined::Primitive(Value::Bool(&built_list[0] <= &built_list[1]));
+                }
+                AllCombined::Primitive(Value::Bool(false))
+            }
+            Opss::MoreEq(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    return AllCombined::Primitive(Value::Bool(&built_list[0] >= &built_list[1]));
+                }
+                AllCombined::Primitive(Value::Bool(false))
+            }
+            Opss::Neq(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    return AllCombined::Primitive(Value::Bool(&built_list[0] != &built_list[1]));
+                }
+                AllCombined::Primitive(Value::Bool(false))
+            }
+            Opss::Plus(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    match (&built_list[0], &built_list[1]) {
+                        (
+                            AllCombined::Primitive(Value::Number(n1)),
+                            AllCombined::Primitive(Value::Number(n2)),
+                        ) => {
+                            let res = n1.as_f64().unwrap_or(0.0) + n2.as_f64().unwrap_or(0.0);
+
+                            return AllCombined::Primitive(Value::Number(
+                                Number::from_f64(res).unwrap(),
+                            ));
+                        }
+                        (
+                            AllCombined::Primitive(Value::String(s1)),
+                            AllCombined::Primitive(Value::String(s2)),
+                        ) => return AllCombined::Primitive(Value::String(format!("{}{}", s1, s2))),
+                        (_, _1) => return AllCombined::Primitive(Value::Null),
+                    }
+                }
+                AllCombined::Primitive(Value::Null)
+            }
+            Opss::Minus(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    match (&built_list[0], &built_list[1]) {
+                        (
+                            AllCombined::Primitive(Value::Number(n1)),
+                            AllCombined::Primitive(Value::Number(n2)),
+                        ) => {
+                            let res = n1.as_f64().unwrap_or(0.0) - n2.as_f64().unwrap_or(0.0);
+
+                            return AllCombined::Primitive(Value::Number(
+                                Number::from_f64(res).unwrap(),
+                            ));
+                        }
+                        (_, _1) => return AllCombined::Primitive(Value::Null),
+                    }
+                }
+                AllCombined::Primitive(Value::Null)
+            }
+            Opss::Multiply(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    match (&built_list[0], &built_list[1]) {
+                        (
+                            AllCombined::Primitive(Value::Number(n1)),
+                            AllCombined::Primitive(Value::Number(n2)),
+                        ) => {
+                            let res = n1.as_f64().unwrap_or(0.0) * n2.as_f64().unwrap_or(0.0);
+
+                            return AllCombined::Primitive(Value::Number(
+                                Number::from_f64(res).unwrap(),
+                            ));
+                        }
+                        (_, _1) => return AllCombined::Primitive(Value::Null),
+                    }
+                }
+                AllCombined::Primitive(Value::Null)
+            }
+            Opss::Division(l) => {
+                if let Some(built_list) = binary_op_vars(l, context) {
+                    match (&built_list[0], &built_list[1]) {
+                        (
+                            AllCombined::Primitive(Value::Number(n1)),
+                            AllCombined::Primitive(Value::Number(n2)),
+                        ) => {
+                            let second = n2.as_f64().unwrap_or(0.0);
+                            if second == 0.0 {
+                                return AllCombined::Primitive(Value::Null);
+                            }
+
+                            let res = n1.as_f64().unwrap_or(0.0) / second;
+
+                            return AllCombined::Primitive(Value::Number(
+                                Number::from_f64(res).unwrap(),
+                            ));
+                        }
+                        (_, _1) => return AllCombined::Primitive(Value::Null),
+                    }
+                }
+                AllCombined::Primitive(Value::Null)
+            }
+            Opss::Var(key) => AllCombined::Primitive(get_context_var(key, &context)),
         }
-
-        let built_list = execute_combined_list(&arguments);
-
-        let left = &built_list[0];
-        let right = &built_list[1];
-
-        let res_bool = match op {
-            Operations::Less => left < right,
-            Operations::More => left > right,
-            Operations::Eq => left == right,
-            Operations::LessEq => left <= right,
-            Operations::MoreEq => left >= right,
-            Operations::Neq => left != right,
-        };
-
-        AllCombined::Primitive(Value::Bool(res_bool))
     }
 }
 
-fn execute_combined_list(l: &Vec<AllCombined>) -> Vec<AllCombined> {
-    l.iter().map(|l_item| l_item.execute()).collect()
+fn execute_combined_list(l: &Vec<AllCombined>, context: &Value) -> Vec<AllCombined> {
+    l.iter().map(|l_item| l_item.execute(context)).collect()
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -74,13 +176,13 @@ enum AllCombined {
 }
 
 impl AllCombined {
-    fn execute(&self) -> AllCombined {
+    fn execute(&self, context: &Value) -> AllCombined {
         match self {
             AllCombined::OpList(l) => {
-                let s: Vec<AllCombined> = execute_combined_list(&l);
+                let s: Vec<AllCombined> = execute_combined_list(&l, context);
                 AllCombined::OpList(s)
             }
-            AllCombined::Ops(o) => o.execute(),
+            AllCombined::Ops(o) => o.execute(context),
             AllCombined::Primitive(v) => AllCombined::Primitive(v.clone()),
         }
     }
@@ -94,6 +196,7 @@ impl std::cmp::Ord for AllCombined {
                 AllCombined::Primitive(Value::Number(n1)),
                 AllCombined::Primitive(Value::Number(n2)),
             ) => {
+                // Todo, compare as i64 too
                 let n1_num = n1.as_f64().unwrap_or(0.0);
                 let n2_num = n2.as_f64().unwrap_or(0.0);
                 if n1_num > n2_num {
@@ -143,11 +246,36 @@ mod tests {
             (r#" { "=" : ["hi","hi2"] }"#, false),
             (r#" { "=" : ["hi", 2] }"#, false),
             (r#" { ">" : ["hi", 2] }"#, false),
+            (r#" { "=" : [{"+": [1,1]}, 2.0] }"#, true),
+            (r#" { "=" : [{"-": [2,1]}, 1.0] }"#, true),
+            (r#" { "=" : [{"*": [2,3]}, 6.0] }"#, true),
+            (r#" { "=" : [{"/": [6,3]}, 2.0] }"#, true),
+            (r#" { "=" : [{"/": [6,0]}, null] }"#, true),
+            (r#" { "=" : [{"var" : "champ.name"}, "Fezzig"] }"#, true),
+            (
+                r#" { "=" : [{"var" : "challenger.name"}, "Dread Pirate Roberts"] }"#,
+                true,
+            ),
+            (r#" { "=" : [{"var" : "rounds"}, 4] }"#, true),
         ];
+
+        let context: Value = serde_json::from_str(
+            r#"{
+            "rounds" : 4, 
+            "champ" : {
+              "name" : "Fezzig",
+              "height" : 223
+            },
+            "challenger" : {
+              "name" : "Dread Pirate Roberts",
+              "height" : 183
+            }
+          }"#,
+        )?;
 
         for (data, expected) in cases {
             let p: AllCombined = serde_json::from_str(data)?;
-            let res = p.execute();
+            let res = p.execute(&context);
             assert_eq!(res, AllCombined::Primitive(Value::Bool(expected)));
         }
         Ok(())
